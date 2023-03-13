d29a5cdc8e8d066eb0ddc93722b85886f27bb279
LacrosR <cmotaromero@gmail.com>
2023-03-13 12:46:07 +0100

    Trabajo en clase

--- practica4.R
+++ practica4.R
@@ -3,9 +3,12 @@ install.packages("LaTeX")
 install.packages("knitr")
 
 library(tinytex)
+library(knitr)
+library(markdown)
 tinytex::install_tinytex()
 
-x <- rnorm(1000)
+
+
 hist(x)
 
 y <- runif(1000)
@@ -19,3 +22,14 @@ rnorm(10)
 
 set.seed(1)
 rnorm(5)
+
+x <- rnorm(1000)
+shapiro.test(x)
+
+z <- rexp(1000)
+shapiro.test(z)
+
+#Shapiro test comprueba si la población de datos tiene una distribución 
+#normal. Esto se mira con el p value, si es superior a 0'05 indica que la 
+#población es probablemente normal. Si no lo fuera, significaría que tiene 
+#una distribución distinta a la normal
